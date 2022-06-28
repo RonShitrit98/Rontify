3820d05033c25987c62ba17f73dfb1ee212ef9b0
Ron Shitrit <ronshitrit456@gmail.com>
2022-06-28 16:29:04 +0300

    player added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPlayerComponent } from './main-player/main-player.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPlayerComponent,
+    MainSidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    AngularSvgIconModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
